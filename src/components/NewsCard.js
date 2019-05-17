305d71567052d74a6b2276e1774691836ddf9420
github username <github email address>
2019-05-17 09:19:24 -0700

    fetch news

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,12 +1,44 @@
 import React, {Component} from "react"
+import { NEWS_URL, HEADERS, CORS} from "../constants"
 
 class NewsCard extends Component {
+  constructor() {
+    super()
+    this.state = {
+      website: null
+    }
+  }
+
+  componentWillMount = () => {
+    this.fetchWebsite()
+  }
+
+  fetchWebsite = () => {
+    fetch(`${CORS}/${NEWS_URL}/${this.props.website}?fields=url`, {
+      headers: HEADERS
+    })
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      website: data[0].url
+    }))
+  }
+
   render() {
+    if (!this.state.website) {
+      return <div />
+    }
     return (
-      <div>
+      <div className="newsCard">
+        <h2>
+          <a href={this.state.website}>
+          {this.props.title}
+          </a>
+        </h2>
+        <img src={this.props.image} alt='img' className="newsImg"></img>
       </div>
     )
   }
+
 }
 
 export default NewsCard
--- src/components/NewsContainer.js
+++ src/components/NewsContainer.js
@@ -1,10 +1,48 @@
 import React, {Component} from "react"
 import NewsCard from "./NewsCard"
+import { NEWS_API, HEADERS, CORS} from "../constants"
 
 class NewsContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      articles: []
+    }
+  }
+
+  componentWillMount = () => {
+    this.fetchArticles()
+  }
+
+  fetchArticles = () => {
+    fetch(`${CORS}/${NEWS_API}/?fields=title,image,website&order=published_at:desc`, {
+      headers: HEADERS
+    })
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      articles: data
+    }))
+  }
+
+  createArticles = () => {
+    return this.state.articles.map(article =>
+      <NewsCard
+        article={article}
+        key={article.id}
+        title={article.title}
+        image={article.image}
+        website={article.website}
+      />
+    )
+  }
+
   render() {
     return (
       <div>
+        <h1>Recent News</h1>
+        <div>
+          {this.createArticles()}
+        </div>
       </div>
     )
   }
